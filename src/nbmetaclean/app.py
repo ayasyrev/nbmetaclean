9d795c6af57f288d2b81171b1274435c621318b9
ayasyrev <a.yasyrev@gmail.com>
2023-10-25 17:16:25 +0300

    timestamp to app

--- src/nbmetaclean/app.py
+++ src/nbmetaclean/app.py
@@ -17,6 +17,10 @@ class AppCfg:
         default=nbformat.NO_CONVERT, help="Save as version, default - no convert"
     )
     silent: bool = field_argument("-s", default=False, action="store_true")
+    not_pt: bool = field_argument(
+        default=False,
+        help="Do not preserve timestamp, default - preserve timestamp",
+        action="store_true")
 
 
 app = App(
@@ -32,13 +36,19 @@ def clean(
     """Clean metadata and execution_count from Jupyter notebook."""
     path_list = cfg.path if isinstance(cfg.path, list) else [cfg.path]
     nb_files: list[Path] = []
+    print(cfg)
     for path in path_list:
         try:
             nb_files.extend(get_nb_names(path))
         except FileNotFoundError:
             rprint(f"{path} not exists!")
     rprint(f"find notebooks: {len(nb_files)} ")
-    cleaned = clean_nb_file(nb_files, as_version=cfg.as_version, silent=cfg.silent)
+    cleaned = clean_nb_file(
+        nb_files,
+        as_version=cfg.as_version,
+        silent=cfg.silent,
+        preserve_timestamp=not cfg.not_pt,
+    )
     rprint(f"cleaned nbs: {len(cleaned)}")
 
 
