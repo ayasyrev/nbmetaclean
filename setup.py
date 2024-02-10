50560c729f50322883292218012d1df20e5080a4
ayasyrev <a.yasyrev@gmail.com>
2024-02-10 09:40:23 +0000

    black

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 from setuptools import setup
 
-
-REQUIREMENTS_FILENAME = 'requirements.txt'
-REQUIREMENTS_TEST_FILENAME = 'requirements_test.txt'
+REQUIREMENTS_FILENAME = "requirements.txt"
+REQUIREMENTS_TEST_FILENAME = "requirements_test.txt"
 
 
 # Requirements
