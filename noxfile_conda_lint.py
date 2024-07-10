b7f6523be4dfdf197f379ac78e739fdc30a1842c
ayasyrev <a.yasyrev@gmail.com>
2024-07-10 08:46:46 +0000

    uv + ruff at nox

--- noxfile_conda_lint.py
+++ noxfile_conda_lint.py
@@ -6,5 +6,5 @@ locations = "src/nbmetaclean", "tests", "noxfile.py"
 @nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12"], venv_backend="mamba")
 def conda_lint(session: nox.Session) -> None:
     args = session.posargs or locations
-    session.conda_install("flake8")
-    session.run("flake8", *args)
+    session.conda_install("ruff")
+    session.run("ruff", "check", *args)
