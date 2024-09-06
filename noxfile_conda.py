0363632a8aa6d3cdfb08649a26b716e9067f4f28
ayasyrev <a.yasyrev@gmail.com>
2024-09-06 11:46:34 +0300

    Fix nox - set default venv backend and reuse existing virtualenvs, update session decorators.

--- noxfile_conda.py
+++ noxfile_conda.py
@@ -1,9 +1,12 @@
 import nox
 
+nox.options.default_venv_backend = "mamba|conda"
+nox.options.reuse_existing_virtualenvs = True
 
-@nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12"], venv_backend="mamba")
+
+@nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12"])
 def conda_tests(session: nox.Session) -> None:
     args = session.posargs or ["--cov"]
     session.conda_install("uv")
-    session.install("uv", "pip", "install", "-e", ".[test]")
+    session.run("uv", "pip", "install", "-e", ".[test]")
     session.run("pytest", *args)
